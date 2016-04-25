ba749861ed46bff9cf073e584caf60c4e394526d
edufm <eduferrarimagalhaes@hotmail.com>
2016-04-25 16:50:39 -0300

    Base bem tosca das classes que vamos usar

--- Main.py
+++ Main.py
@@ -2,6 +2,28 @@
 """
 Created on Mon Apr 25 16:03:18 2016
 
-@author: Jean Walper
+@author: Eh Nòis
 """
+import numpy as np
 
+class Mapa():
+    def __init__(self, matriz):
+        self.matriz = np.zeros([15,15])
+        
+class Player():
+    def __init__(self, health, weapon):
+        self.health = 20
+        self.weapon = None
+
+class Gun():
+    def __init__(self, ID, Ammo, Range, Damage):
+        self.ID = ID 
+        self.Ammo = Ammo
+        self.Range = Range
+        self.Damage = Damage
+
+class Enemys():
+    def __init__(self, ID, health, damage):
+        self.ID = ID        
+        self.health = health
+        self.damage = damage
