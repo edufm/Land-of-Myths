3b4226af22e3c3661cd7ae61adf311c8156e3fac
edufm <eduferrarimagalhaes@hotmail.com>
2016-04-27 20:32:08 -0300

    Adicionei todos os botões com 2X2 de medida
    Não achei jeito menos porco que uma lista gigante, mas funciona e com a
    função "Create_Botton" que passa atributos pro botão ta fácil de editar
    o que eles fazem

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ Created on Mon Apr 25 16:03:18 2016
 
 @author: Eh Nòis
 """
+
 from tkinter import *
 import numpy as np
 
@@ -13,12 +14,11 @@ from ClasseGun import Gun
 from ClasseMapa import Mapa
 from ClassePlayer import Player
 
-
-        
+               
 window = Tk()
 window.title("Não sabemos ainda")
 window.geometry("1366x728")
-
-Mapa.load_map()
+               
+Mapa.load_map(window)
 
 window.mainloop()
