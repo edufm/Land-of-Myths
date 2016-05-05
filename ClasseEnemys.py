0e5eea7b012f579ee0ce26dc6cf7451d2bc1d100
edufm <eduferrarimagalhaes@hotmail.com>
2016-05-05 09:08:45 -0300

    Demência art. evolui para: inteligência art.
    agora tem alguma inteligência artificial funcionado, mas ainda o combate
    não está calibrado, e ainda não é possível matar os zumbis

--- ClasseEnemys.py
+++ ClasseEnemys.py
@@ -32,39 +32,39 @@ class Enemys():
         pass
     
     def left(self):
-        self.pos[0] -= 1
-    def right(self):
-        self.pos[0] += 1
-    def up(self):
         self.pos[1] -= 1
-    def down(self):
+    def right(self):
         self.pos[1] += 1
+    def up(self):
+        self.pos[0] -= 1
+    def down(self):
+        self.pos[0] += 1
         
     def jogada(self, pl, Map, img, imgE):
         distanciax = pl.pos[1] - self.pos[1]
         distanciay = pl.pos[0] - self.pos[0]
         if (distanciax == 0 and distanciay == 1) or (distanciax == 1 and distanciay == 0):
-           self.atack(pl)            
+           self.atack(pl)
         
-        elif distanciax > 0 and abs(distanciax) > abs(distanciay):
+        elif distanciax > 0 and abs(distanciax) > abs(distanciay) and Map.matriz[self.pos[0]][self.pos[1]+1] == 0:
             Map.matriz[self.pos[0]][self.pos[1]] = 0
             Map.b[self.pos[0]][self.pos[1]].config(image=img)
             Map.b[self.pos[0]][self.pos[1]].image = img
-            self.left()
+            self.right()
             Map.matriz[self.pos[0]][self.pos[1]] = 2
             Map.b[self.pos[0]][self.pos[1]].config(image=imgE)
             Map.b[self.pos[0]][self.pos[1]].image = imgE
             
-        elif distanciax < 0 and abs(distanciax) > abs(distanciay):
+        elif distanciax < 0 and abs(distanciax) > abs(distanciay) and Map.matriz[self.pos[0]][self.pos[1]-1] == 0:
             Map.matriz[self.pos[0]][self.pos[1]] = 0
             Map.b[self.pos[0]][self.pos[1]].config(image=img)
             Map.b[self.pos[0]][self.pos[1]].image = img            
-            self.right()
+            self.left()
             Map.b[self.pos[0]][self.pos[1]].config(image=imgE)
             Map.b[self.pos[0]][self.pos[1]].image = imgE
             Map.matriz[self.pos[0]][self.pos[1]] = 2
             
-        elif distanciay > 0 and abs(distanciay) > abs(distanciax):
+        elif distanciay > 0 and abs(distanciay) > abs(distanciax) and Map.matriz[self.pos[0]]+1[self.pos[1]+1] == 0:
             Map.matriz[self.pos[0]][self.pos[1]] = 0
             Map.b[self.pos[0]][self.pos[1]].config(image=img)
             Map.b[self.pos[0]][self.pos[1]].image = img
@@ -73,11 +73,40 @@ class Enemys():
             Map.b[self.pos[0]][self.pos[1]].image = imgE
             Map.matriz[self.pos[0]][self.pos[1]] = 2
             
-        elif distanciay < 0 and abs(distanciay) > abs(distanciax):
+        elif distanciay < 0 and abs(distanciay) > abs(distanciax) and Map.matriz[self.pos[0]-1][self.pos[1]] == 0:
             Map.matriz[self.pos[0]][self.pos[1]] = 0            
             Map.b[self.pos[0]][self.pos[1]].config(image=img)
             Map.b[self.pos[0]][self.pos[1]].image = img
             self.up()
+            Map.b[self.pos[0]][self.pos[1]].config(image=imgE)
+            Map.b[self.pos[0]][self.pos[1]].image = imgE
+            Map.matriz[self.pos[0]][self.pos[1]] = 2
+            
+        elif abs(distanciay) == abs(distanciax):
+            Map.matriz[self.pos[0]][self.pos[1]] = 0
+            Map.b[self.pos[0]][self.pos[1]].config(image=img)
+            Map.b[self.pos[0]][self.pos[1]].image = img
+            Op = ran.randint(1,3)      
+            if distanciay < 0 and distanciax < 0 and Map.matriz[self.pos[0]-1][self.pos[1]] == 0 and Map.matriz[self.pos[0]][self.pos[1]-1] == 0:
+                if Op == 1:
+                    self.left()
+                if Op == 2:
+                    self.up()
+            if distanciay > 0 and distanciax < 0 and Map.matriz[self.pos[0]+1][self.pos[1]] == 0 and Map.matriz[self.pos[0]][self.pos[1]-1] == 0:
+                if Op == 1:
+                    self.left()
+                if Op == 2:
+                    self.down()
+            if distanciay < 0 and distanciax > 0 and Map.matriz[self.pos[0]-1][self.pos[1]] == 0 and Map.matriz[self.pos[0]][self.pos[1]+1] == 0:
+                if Op == 1:
+                    self.right()
+                if Op == 2:
+                    self.up()
+            if distanciay > 0 and distanciax > 0 and Map.matriz[self.pos[0]+1][self.pos[1]] == 0 and Map.matriz[self.pos[0]][self.pos[1]-1] == 0:
+                if Op == 1:
+                    self.right()
+                if Op == 2:
+                    self.down()
             Map.b[self.pos[0]][self.pos[1]].config(image=imgE)
             Map.b[self.pos[0]][self.pos[1]].image = imgE
             Map.matriz[self.pos[0]][self.pos[1]] = 2
