7221c855c4a73514b5f2d643135387e46e26f672
hugoeflavia <hugo.mendessp@yahoo.com.br>
2016-05-06 11:55:03 -0300

    atualizei a classe imagens

--- Imagens/ClasseImagens.py
+++ Imagens/ClasseImagens.py
@@ -13,17 +13,17 @@ class Imagem:
         self.ImagemEsq = ImagemEsq
         self.ImagemCost = ImagemCost
         
-enemy1_front = PhotoImage(file=".//Imagens//enemy1_front.png")
-enemy1_right = PhotoImage(file=".//Imagens//enemy1_right.png")
-enemy1_left = PhotoImage(file=".//Imagens//enemy1_left.png")
-enemy1_back = PhotoImage(file=".//Imagens//enemy1_back.png")
+enemy1_front = PhotoImage(file=".//Imagens//Sprites//enemy1_front.png")
+enemy1_right = PhotoImage(file=".//Imagens//Sprites//enemy1_right.png")
+enemy1_left = PhotoImage(file=".//Imagens//Sprites//enemy1_left.png")
+enemy1_back = PhotoImage(file=".//Imagens//Sprites//enemy1_back.png")
 
 enemy1 = (enemy1_front,enemy1_right,enemy1_left,enemy1_back)   
 
-enemy2_front = PhotoImage(file=".//Imagens//enemy2_front.png")
-enemy2_right = PhotoImage(file=".//Imagens//enemy2_right.png")
-enemy2_left = PhotoImage(file=".//Imagens//enemy2_left.png")
-enemy2_back = PhotoImage(file=".//Imagens//enemy2_back.png")
+enemy2_front = PhotoImage(file=".//Imagens//Sprites//enemy2_front.png")
+enemy2_right = PhotoImage(file=".//Imagens//Sprites//enemy2_right.png")
+enemy2_left = PhotoImage(file=".//Imagens//Sprites//enemy2_left.png")
+enemy2_back = PhotoImage(file=".//Imagens//Sprites//enemy2_back.png")
 
 enemy2 = (enemy2_front,enemy2_right,enemy2_left,enemy2_back)   
 
