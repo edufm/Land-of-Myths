054d316b96e0d9c459f19a23f3ef46cefb12a7f7
edufm <eduferrarimagalhaes@hotmail.com>
2016-05-16 16:43:30 -0300

    Tem bind!!!!
    WASD = movimento
    X = pular jogada
    1,2,3 = selecionar armas

--- ClasseTrack.py
+++ ClasseTrack.py
@@ -6,14 +6,16 @@ Created on Sun May 15 13:48:42 2016
 """
 
 class Track():
-    def __init__(self, Turn, Enemies, Boss, Weaponselected, Music,Aim):
+    def __init__(self, Turn, Enemies, Boss, Weaponselected, Music, Aim, Map, pl):
         self.Turn = Turn 
         self.Enemies = Enemies
         self.Boss = Boss
         self.Weaponselected = Weaponselected
         self.Music = Music
         self.Aim = Aim #Aim mostra se o jogador tem  intenção e atirar nesse turno
+        self.Map = Map
+        self.pl = pl
         
-Tracker = Track(0, 0, 0, 0, 0, 0)
+Tracker = Track(0, 0, 0, 0, 0, 0, 0, 0)
 
 
