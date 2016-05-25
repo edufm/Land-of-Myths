d594cb28822b3f07f05cdb095715c0a592792c8f
Jean Walper <jeanlow@al.insper.edu.br>
2016-05-25 13:35:17 -0300

    Debug Timer

--- ClasseTimer.py
+++ ClasseTimer.py
@@ -1,9 +1,6 @@
-import threading
-
 import ClasseMapa
 import ClasseTrack
 def Timer(pl,Turn):
-    if Turn == ClasseTrack.Tracker.Turn - 1 or turn == ClasseTrack.Tracker.Turn:
-        print ("=",Turn,ClasseTrack.Tracker.Turn)
+    if Turn == ClasseTrack.Tracker.Turn - 1 or Turn == ClasseTrack.Tracker.Turn:
         ClasseMapa.Mapa.Roda_jogo(ClasseTrack.Tracker.Map,pl)
     
